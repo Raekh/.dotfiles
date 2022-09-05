94951038b129acdaafd8465c9d4a8c21e8d06e4e
Florian Roura <florian.roura@viaco.fr>
2022-09-05 08:52:19 +0200

    [*] nvim - aesthetic changes

--- nvim/.config/nvim/lua/highlights.lua
+++ nvim/.config/nvim/lua/highlights.lua
@@ -5,4 +5,4 @@ vim.opt.wildoptions = 'pum'
 vim.opt.pumblend = 5
 vim.opt.background = 'dark'
 
-vim.cmd('set list listchars=tab:\\│\\ ,trail:·,extends:»,precedes:«,nbsp:×')
+-- vim.cmd('set list listchars=tab:\\│\\ ,trail:·,extends:»,precedes:«,nbsp:×')
