5fd875c4b5687754e5df0c222c94333cc985691d
Florian Roura <florian.roura@viaco.fr>
2022-09-11 23:53:10 +0200

    Added useful plugins (still need sorting)

---
+++ nvim/.config/nvim/plugin/emoji_picker.rc.lua
@@ -0,0 +1,6 @@
+local status, _ = pcall(require, 'emoji_picker')
+if (not status) then return end
+
+-- require'emoji_picker'.setup()
+
+-- vim.keymap.set('i', '<Leader>e', '<Cmd>EmojiPicker<CR>')
---
+++ nvim/.config/nvim/plugin/mind.rc.lua
@@ -0,0 +1,4 @@
+local status,_ = pcall(require, 'mind')
+if (not status) then return end
+
+require'mind'.setup()
