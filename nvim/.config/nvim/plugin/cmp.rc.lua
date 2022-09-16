a499f44d7071f337510524ff8aff4e38d34b6ea4
Florian Roura <florian.roura@viaco.fr>
2022-09-16 20:59:46 +0200

    Added lspsaga descriptors for tabnine

--- nvim/.config/nvim/plugin/cmp.rc.lua
+++ nvim/.config/nvim/plugin/cmp.rc.lua
@@ -4,6 +4,14 @@ if (not status) then return end
 
 local lspkind = require 'lspkind'
 
+local source_mapping = {
+    buffer = "[Buffer]",
+    nvim_lsp = "[LSP]",
+    nvim_lua = "[Lua]",
+    cmp_tabnine = "[TN]",
+    path = "[Path]",
+}
+
 cmp.setup({
     snippet = {
         expand = function(args)
@@ -26,7 +34,24 @@ cmp.setup({
         { name = 'cmp_tabnine' },
     }),
     formatting = {
-        format = lspkind.cmp_format({ with_text = false, maxwidth = 50 })
+        format = function (entry, vim_item)
+            vim_item.kind = lspkind.symbolic(vim_item.kind, {mode = "symbol"})
+            vim_item.menu = source_mapping[entry.source.name]
+            if entry.source.nabe == "cmp_tabnine" then
+                local detail = (entry.completion_item.data or {}).detail
+                vim_item.kind = ""
+                if detail and detail:find('.*%%.*') then
+                    vim_item.kind = vim_item.kind .. ' ' .. detail
+                end
+
+                if (entry.completion_item.data or {}).multiline then
+                    vim_item.kind = vim_item.kind .. ' ' .. '[ML]'
+                end
+            end
+            local maxwidth = 80
+            vim_item.abbr = string.sub(vim_item.abbr, 1, maxwidth)
+            return vim_item
+        end
     }
 })
 
