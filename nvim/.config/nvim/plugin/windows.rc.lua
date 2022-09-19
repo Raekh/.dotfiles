0b57896db1b87f6e27975f573db5856d215d2386
Florian Roura <florian.roura@viaco.fr>
2022-09-19 18:53:09 +0200

    Added windows plugin

---
+++ nvim/.config/nvim/plugin/windows.rc.lua
@@ -0,0 +1,26 @@
+local status, w = require('windows')
+if (not status) then return end
+
+require('windows').setup({
+    autowidth = {
+        -- filetype = {
+        --     DiffviewFiles = 5,
+        -- },
+    },
+    ignore = {
+        -- buftype = { "diffview.nvim" },
+        filetype = { "NvimTree", "neo-tree", "undotree", "gundo", "DiffviewFiles" }
+    },
+    animation = {
+        enable = true,
+        duration = 300,
+        fps = 60,
+        easing = "in_out_sine"
+    }
+})
+
+vim.o.winwidth = 10
+vim.o.winminwidth = 10
+vim.o.equalalways = false
+
+vim.keymap.set('n', ';wz', '<Cmd>WindowsMaximaze<CR>')
