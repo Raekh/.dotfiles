e96acfdfebd9a25a4cae0fa46f51e4df99747bbb
Florian Roura <florian.roura@viaco.fr>
2023-01-19 19:41:56 +0100

    Finally some good fucking food

--- nvim/.config/nvim/lua/plugins/qol.lua
+++ nvim/.config/nvim/lua/plugins/qol.lua
@@ -1,7 +1,42 @@
 return {
 	"kazhala/close-buffers.nvim",
 	"aduros/ai.vim",
-	"lukas-reineke/indent-blankline.nvim",
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		opts = {
+			show_current_context = true,
+			show_current_context_start = true,
+		},
+		init = function()
+			vim.opt.list = true
+			vim.g.indent_blankline_filetype_exclude = {
+				"help",
+				"startify",
+				"dashboard",
+				"alpha",
+				"packer",
+				"NeogitStatus",
+				"NeogitCommitView",
+				"NeogitPopup",
+				"NeogitLogView",
+				"NeogitCommitMessage",
+				"man",
+				"sagasignature",
+				"sagahover",
+				"lspsagafinder",
+				"LspSagaCodeAction",
+				"TelescopePrompt",
+				"NvimTree",
+				"Trouble",
+				"DiffviewFiles",
+				"DiffviewFileHistory",
+				"Outline",
+				"lspinfo",
+				"fugitive",
+				"norg",
+			}
+		end,
+	},
 	"NMAC427/guess-indent.nvim",
 	{
 		"kyazdani42/nvim-web-devicons",
@@ -10,4 +45,14 @@ return {
 			default = true,
 		},
 	},
+	{
+		"nacro90/numb.nvim",
+		opts = {
+			show_numbers = true, -- Enable 'number' for the window while peeking
+			show_cursorline = true, -- Enable 'cursorline' for the window while peeking
+			number_only = false, -- Peek only when the command is only a number instead of when it starts with a number
+			centered_peeking = true, -- Peeked line will be centered relative to window
+		},
+	},
+	{ "folke/todo-comments.nvim", config = true },
 }
