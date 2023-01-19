0611384e6d8c525aaa3d3862857f72f8c6e0448f
Florian Roura <florian.roura@viaco.fr>
2023-01-19 19:16:43 +0100

    guess-indent

--- nvim/.config/nvim/plugin/guess-indent.rc.lua
+++ nvim/.config/nvim/plugin/guess-indent.rc.lua
@@ -1,3 +1,10 @@
-import('guess-indent', function(gi)
-    gi.setup()
-end)
+import("guess-indent", function(gi)
+	gi.setup({
+		auto_cmd = true,
+	})
+
+	vim.api.nvim_create_autocmd("VimEnter", {
+		group = vim.api.nvim_create_augroup("GuessIndent", { clear = true }),
+		command = "autocmd BufReadPost * :silent lua require('guess-indent').set_from_buffer(true)",
+	})
+end, {})
