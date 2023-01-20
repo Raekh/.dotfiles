74efcc0cb8321ed8da1bbfce41e9283de6216e88
Florian Roura <florian.roura@viaco.fr>
2023-01-20 13:31:17 +0100

    folkedots

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -1,64 +1,65 @@
 return {
-    "akinsho/nvim-bufferline.lua",
-    lazy = false,
-    keys = {
-        {'<C-l>', '<cmd>BufferLineCycleNext<CR>'},
-        {'<C-h>', '<cmd>BufferLineCyclePrev<CR>'},
-        {'<C-x>', '<cmd>Bdelete<CR>'}
-    },
-    config = function()
-        local symbol_map = {
-            error = "",
-            warning = "",
-            info = "",
-            hint = "",
-        }
+	"akinsho/nvim-bufferline.lua",
+	lazy = false,
+	priority = 1000,
+	keys = {
+		{ "<C-l>", "<cmd>BufferLineCycleNext<CR>" },
+		{ "<C-h>", "<cmd>BufferLineCyclePrev<CR>" },
+		{ "<C-x>", "<cmd>Bdelete<CR>" },
+	},
+	config = function()
+		local symbol_map = {
+			error = "",
+			warning = "",
+			info = "",
+			hint = "",
+		}
 
-        require('bufferline').setup {
-            options = {
-                mode = 'buffers',
-                numbers = 'none',
-                show_tab_indicators = true,
-                max_name_length = 30,
-                max_prefix_length = 20,
-                diagnostics = "nvim_lsp",
-                indicator = {
-                    icon = '▎', -- this should be omitted if indicator style is not 'icon'
-                    -- separator_style = { "│", "│" },
-                    style = 'icon',
-                },
-                ---@diagnostic disable-next-line: unused-local
-                diagnostics_indicator = function(total_count, level, diagnostics_dict, context)
-                    -- local icon = level:match("error") and " " or " "
-                    -- return " " .. icon .. count
-                    local s = ""
-                    for kind, count in pairs(diagnostics_dict) do
-                        s = string.format("%s %s %d", s, symbol_map[kind], count)
-                    end
-                    return s
-                end,
-                diagnostics_update_in_insert = true,
-                name_formatter = function(buf)
-                    if buf.name:match('%.md') then
-                        return vim.fn.fnamemodify(buf.name, ':t:r')
-                    end
-                end,
-                ---@diagnostic disable-next-line: assign-type-mismatch
-                separator_style = { "│", "│" },
-                always_show_bufferline = true,
-                show_buffer_close_icons = true,
-                show_close_icon = false,
-                color_icons = true,
-                offsets = {
-                    {
-                        filetype = "NvimTree",
-                        text = "File Explorer",
-                        text_align = "center",
-                        separator = true
-                    }
-                },
-            },
-            --[[
+		require("bufferline").setup({
+			options = {
+				mode = "buffers",
+				numbers = "none",
+				show_tab_indicators = true,
+				max_name_length = 30,
+				max_prefix_length = 20,
+				diagnostics = "nvim_lsp",
+				indicator = {
+					icon = "▎", -- this should be omitted if indicator style is not 'icon'
+					-- separator_style = { "│", "│" },
+					style = "icon",
+				},
+				---@diagnostic disable-next-line: unused-local
+				diagnostics_indicator = function(total_count, level, diagnostics_dict, context)
+					-- local icon = level:match("error") and " " or " "
+					-- return " " .. icon .. count
+					local s = ""
+					for kind, count in pairs(diagnostics_dict) do
+						s = string.format("%s %s %d", s, symbol_map[kind], count)
+					end
+					return s
+				end,
+				diagnostics_update_in_insert = true,
+				name_formatter = function(buf)
+					if buf.name:match("%.md") then
+						return vim.fn.fnamemodify(buf.name, ":t:r")
+					end
+				end,
+				---@diagnostic disable-next-line: assign-type-mismatch
+				separator_style = { "│", "│" },
+				always_show_bufferline = true,
+				show_buffer_close_icons = true,
+				show_close_icon = false,
+				color_icons = true,
+				offsets = {
+					{
+						filetype = "NvimTree",
+						text = "File Explorer",
+						text_align = "center",
+						separator = true,
+					},
+				},
+			},
+			--[[
             highlights = {
                 separator = {
                     guifg = '#073642',
@@ -80,6 +81,6 @@ return {
                 }
             }
         --]]
-        }
-    end
+		})
+	end,
 }
