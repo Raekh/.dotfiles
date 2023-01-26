3ad2e07c507668b23e3c915dbca7269b15509e33
Florian Roura <florian.roura@viaco.fr>
2023-01-26 21:03:52 +0100

    ts-node-action

--- nvim/.config/nvim/lua/plugins/misc.lua
+++ nvim/.config/nvim/lua/plugins/misc.lua
@@ -1,7 +1,19 @@
 return {
-    "andymass/vim-matchup",
-    event = "BufReadPost",
-    config = function()
-        vim.g.matchup_matchparen_offscreen = { method = "status_manual" }
-    end,
+	{
+		"CKolkey/ts-node-action",
+		dependencies = {
+			"nvim-treesitter",
+		},
+		keys = {
+			{ ";ts", "NodeAction" },
+		},
+		config = true,
+	},
+	{
+		"andymass/vim-matchup",
+		event = "BufReadPost",
+		config = function()
+			vim.g.matchup_matchparen_offscreen = { method = "status_manual" }
+		end,
+	},
 }
