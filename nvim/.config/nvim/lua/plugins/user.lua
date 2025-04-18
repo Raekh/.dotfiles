1d26baa624f18121e5f80e8680c1fc8da1c231fb
Ostrogoth <florian.roura@onceforall.com>
2025-04-18 17:25:28 +0200

    [nvim] some plugins + last commit before astro v5

--- nvim/.config/nvim/lua/plugins/user.lua
+++ nvim/.config/nvim/lua/plugins/user.lua
@@ -120,6 +120,15 @@ return {
       { "<leader>lH", "<cmd>InlayHintsToggle<CR>", desc = "Toggle Inlay Hints" },
     },
   },
+  {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "VeryLazy", -- Or `LspAttach`
+    priority = 1000, -- needs to be loaded in first
+    config = function()
+      require("tiny-inline-diagnostic").setup()
+      -- vim.diagnostic.config { virtual_text = false } -- Only if needed in your configuration, if you already have native LSP diagnostics
+    end,
+  },
   -- {
   --   "pmizio/typescript-tools.nvim",
   --   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
