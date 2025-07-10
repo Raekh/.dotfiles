4bef0a0485d70357b615bd0c4d3d7f835e568be0
Ostrogoth <florian.roura@onceforall.com>
2025-07-10 20:33:04 +0200

    [nvim] plugin and config updates

--- nvim/.config/nvim/lua/plugins/filetypes.lua
+++ nvim/.config/nvim/lua/plugins/filetypes.lua
@@ -1,18 +1,22 @@
 --- @type LazySpec
 return {
   -- markdown
-  {
-    "MeanderingProgrammer/markdown.nvim",
-    name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    ft = { "markdown", "codecompanion" },
-    config = function() require("render-markdown").setup {} end,
-  },
+  -- {
+  --   "MeanderingProgrammer/markdown.nvim",
+  --   name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
+  --   dependencies = { "nvim-treesitter/nvim-treesitter" },
+  --   ft = { "markdown", "codecompanion" },
+  --   config = function() require("render-markdown").setup {} end,
+  -- },
 
   -- Render markdown in the chat buffer
   {
     "OXY2DEV/markview.nvim",
     lazy = false,
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "MeanderingProgrammer/markdown.nvim",
+    },
     opts = {
       preview = {
         filetypes = { "markdown", "codecompanion" },
