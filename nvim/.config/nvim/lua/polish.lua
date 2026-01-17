0cfacd891df32d39d21940e83a2e7cd662e06698
Raekh <encryptedhex@gmail.com>
2026-01-17 14:59:33 +0100

    [nvim] add wsl clipboard

--- nvim/.config/nvim/lua/polish.lua
+++ nvim/.config/nvim/lua/polish.lua
@@ -1,4 +1,30 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- vim:fileencoding=utf-8:foldmethod=marker
+
+-- WSL Clipboard {{{
+local function is_wsl()
+  local version_file = io.open("/proc/version", "rb")
+  if version_file ~= nil and string.find(version_file:read "*all", "microsoft") then
+    version_file:close()
+    return true
+  end
+  return false
+end
+
+if is_wsl() then
+  vim.g.clipboard = {
+    name = "wsl-clipboard",
+    copy = {
+      ["+"] = "wcopy",
+      ["*"] = "wcopy",
+    },
+    paste = {
+      ["+"] = "wpaste",
+      ["*"] = "wpaste",
+    },
+    cache_enabled = 0,
+  }
+end
+-- }}}
 
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
